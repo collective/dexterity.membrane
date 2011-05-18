b5d716b2b15a37d2c0b3740f259e6a246fb52463
Maurits van Rees <maurits@vanrees.org>
2011-05-18 19:26:13 +0000

    Us the Python standard library doctest module
    svn path=/dexterity.membrane/trunk/; revision=239519

--- dexterity/membrane/tests/test_doctest.py
+++ dexterity/membrane/tests/test_doctest.py
@@ -1,21 +1,22 @@
 import unittest
 
-from zope.testing import doctestunit
+#from zope.testing import doctestunit
+import doctest
 from zope.component import testing
-from Testing import ZopeTestCase as ztc
+#from Testing import ZopeTestCase as ztc
 
-from dexterity.membrane.tests import base
+#from dexterity.membrane.tests import base
 
 
 def test_suite():
     return unittest.TestSuite([
 
         # Unit tests
-        #doctestunit.DocFileSuite(
+        #doctest.DocFileSuite(
         #    'README.txt', package='dexterity.membrane',
         #    setUp=testing.setUp, tearDown=testing.tearDown),
 
-        doctestunit.DocTestSuite(
+        doctest.DocTestSuite(
             module='dexterity.membrane.content.member',
             setUp=testing.setUp, tearDown=testing.tearDown),
 

9e1988a728bd58890ed282553e67463bfcb57d81
Maurits van Rees <maurits@vanrees.org>
2013-04-04 11:41:28 +0200

    Revert " new add po files"
    This reverts commit 689d846786472367badbdfd498e78dd926625c9e.

--- dexterity/membrane/patch/membership.py
+++
@@ -1,8 +0,0 @@
-from dexterity.membrane.events import CreateMembraneEvent
-from zope import event    
-def handle_join_success(self, data):
-        # portal should be acquisition wrapped, this is needed for the schema
-        # adapter below
-#        import pdb
-#        pdb.set_trace()
-        event.notify(CreateMembraneEvent(data['fullname'],data['password'],data['email'],data['password_ctl']))
